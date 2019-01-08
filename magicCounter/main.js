79fd1cf393633779d6b20fb427a2bccecc58b063
valor323 <caine101@mail.chapman.edu>
2019-01-08 11:27:27 -0800

    added plus and minus functionality

--- magicCounter/main.js
+++ magicCounter/main.js
@@ -5,20 +5,21 @@ function initializeApp(){
     addClickHandlers();
 };
 
+//global Variables
+let startingLife = null;
+
 function addClickHandlers(){
     $('.players').on('click', selectNumberOfPlayers)
-    $('.life').on('click', selectNumberOfPlayers)
+    $('.life').on('click', selectStartingLife)
     $('.start-match').on('click', initializeLifePage)
     $('.end-match').on('click', backToMenu)
+    $('.plus').on('click', increaseLifeTotal)
+    $('.minus').on('click', decreaseLifetotal)
 }
 
 
 function selectNumberOfPlayers(){
     $('.players').css({
-        'background-color': '#311b92',
-        'color': 'white'
-    });
-    $('.life').css({
         'background-color': '#311b92',
         'color': 'white'
     });
@@ -28,7 +29,21 @@ function selectNumberOfPlayers(){
         });
 }
 
+function selectStartingLife(){
+
+    startingLife = $(this).attr("life");
+    $('.life').css({
+        'background-color': '#311b92',
+        'color': 'white'
+    });
+    $(this).css({
+        'background-color':'yellow',
+        'color': 'black'
+       });
+}
+
 function initializeLifePage(){
+    $('.life-total').text(startingLife)
     $('.setup-container').removeClass('visible')
     $('.setup-container').addClass('none')
     $('.life-counter-container').addClass('visible')
@@ -39,5 +54,20 @@ function backToMenu(){
     $('.life-counter-container').addClass('none')
     $('.setup-container').addClass('visible')
     addClickHandlers();
+}
+
+function increaseLifeTotal(){
+    let parentElement = $(this).parent().find('div')[1]
+    let playersLife = $(this).parent().find('div')[1].innerText
+    playersLife++
+    $(parentElement).text(playersLife)
+    
+}
+
+function decreaseLifetotal(){
+    let childElement = $(this).next()[0]
+    let playersLife =  $(this).next()[0].innerText
+    playersLife--
+    $(childElement).text(playersLife)
 }
 
